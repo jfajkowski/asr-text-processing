cdd577d1ff4e6a6b39cc3247f004df3f6d179925
jfajkowski <szczekalem213>
2017-12-04 18:35:07 +0100

    Some changes.

--- internal/check_requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for file in $@; do
-    if [ ! -f $file ]; then
-        log.sh -ent "File $file is required!"
-        exit 1
-    fi
-done
--- internal/strip_extension.sh
+++ internal/strip_extension.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-perl -pe "s/\.[^.]+$//"
+strip_specific_extension() {
+    local extension=${1}
+    while read filename; do
+        basename ${filename} ${extension}
+    done
+}
+
+strip_any_extension() {
+    perl -pe "s/\.[^.]+$//"
+}
+
+main() {
+    extension=${1}
+    if [ "${extension}" != "" ]; then
+        strip_specific_extension ${extension}
+    else
+        strip_any_extension
+    fi
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
